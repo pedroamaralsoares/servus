2978e73c0205e2553467012207bea0d0f3ea14e2
pedroamaralsoares <pedro.a.soares@tecnico.ulisboa.pt>
2019-11-18 23:48:41 +0000

    added sounds and wet floor

---
+++ Assets/Scripts/DestroyAfterAwake.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyAfterAwake : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float timeToDestroy = 3f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToDestroy -= Time.deltaTime;
+        if (timeToDestroy <= 0f) {
+            Destroy(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/VirtualFloor.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VirtualFloor : MonoBehaviour
+{
+    bool triggered;
+    Vector3 latestPlayerPos;
+    float timeToRest = 0.5f; // time to rest since last sound emmited (3 sec for each sample)
+    public Transform soundSamplePrefab;
+
+    public AudioClip[] clips;
+    private int numClips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        latestPlayerPos = new Vector3(0,0,0);
+        numClips = clips.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeToRest >= 0f && timeToRest < 3f) timeToRest += Time.deltaTime;
+    }
+
+    void Awake()
+    {
+        triggered = false;
+    }
+
+    void OnTriggerEnter (Collider collider) {
+
+        latestPlayerPos = collider.transform.position;
+
+        if (collider.gameObject.tag == "Player")
+        {
+            Transform newSoundObject = Instantiate(soundSamplePrefab, latestPlayerPos, collider.transform.rotation);
+            newSoundObject.GetComponent<AudioSource>().clip = randomClip();
+            newSoundObject.GetComponent<AudioSource>().Play();
+            timeToRest = 0f;
+            Debug.Log("HI");
+        }
+    }
+    void OnTriggerStay(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player")
+        {
+            if (latestPlayerPos == collider.transform.position) return;
+            latestPlayerPos = collider.transform.position;
+            //Debug.Log(latestPlayerPos);
+
+            if (timeToRest >= 0.5f) {
+                Transform newSoundObject = Instantiate(soundSamplePrefab, latestPlayerPos, collider.transform.rotation);
+                newSoundObject.GetComponent<AudioSource>().clip = randomClip();
+                newSoundObject.GetComponent<AudioSource>().Play();
+                timeToRest = 0f;
+            }
+        }
+    }
+
+    void OnTriggerExit (Collider collider) {
+
+    }
+
+    AudioClip randomClip () {
+        return clips[Random.Range(0,numClips-1)];
+    }
+}
