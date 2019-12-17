8c99d71a98645431986469ba7659b8ce8b17b3bb
pedroamaralsoares <pedro.a.soares@tecnico.ulisboa.pt>
2019-12-17 20:44:58 +0000

    orbit neon changed for video

---
+++ Assets/Scripts/OrbitNeon.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrbitNeon : MonoBehaviour
+{
+    private float posY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        posY = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(0, 1, 0, Space.World);
+        transform.position = Vector3.Lerp(transform.position, new Vector3(transform.position.x, posY + Mathf.PingPong(Time.time, 2), transform.position.z) , 3 * Time.deltaTime);
+    }
+}
