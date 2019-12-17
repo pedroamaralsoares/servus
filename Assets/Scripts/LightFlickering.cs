6688aad54d3a5127fea863a208996ef54dee23b0
pedroamaralsoares <pedro.a.soares@tecnico.ulisboa.pt>
2019-12-17 19:06:36 +0000

    tunnel scene added

---
+++ Assets/Scripts/LightFlickering.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightFlickering : MonoBehaviour
+{
+    private Material material;
+    public float timeCycle = 2;
+
+    public Color color1;
+    public Color color2;
+
+
+    void Start()
+    {
+        material = GetComponent<MeshRenderer>().material;    
+    }
+
+    void Update()
+    {
+        Color newColor = Color.Lerp(color1,color2,Mathf.PingPong(Time.time, timeCycle));
+        
+        material.SetColor("_EmissionColor", newColor);
+
+    }
+}
